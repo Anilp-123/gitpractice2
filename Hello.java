7eaedc7d8e89320cb6537efd6a3ec0074257ccd3
anil <madhuanil25@gmail.com>
2022-10-18 12:36:04 +0000

    changed fonts to orange

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
 Public static void main(9String args[])
 {
-System.out.printIn("hello from master origin");
+System.out.printIn("hello from intellipaat changed fonts to to orange");
 }
 }
